a0e863da1f9c1f63bf24ba0e30072c0f43ff6775
arhor <arhor18@gmail.com>
2023-06-05 14:06:26 +0200

    ExtraData WIP;

--- app-server/src/main/kotlin/com/github/mburyshynets/dgs/aop/MethodExecutionLoggingAspect.kt
+++ app-server/src/main/kotlin/com/github/mburyshynets/dgs/aop/MethodExecutionLoggingAspect.kt
@@ -1,66 +1,111 @@
 package com.github.mburyshynets.dgs.aop
 
-import org.aspectj.lang.JoinPoint
+import com.github.mburyshynets.dgs.aop.MethodExecutionLoggingAspect.Companion.PROP_NAME
+import com.github.mburyshynets.dgs.aop.MethodExecutionLoggingAspect.Companion.PROP_VALUE
+import jakarta.annotation.PostConstruct
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
 import org.aspectj.lang.reflect.MethodSignature
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTimedValue
+import java.util.concurrent.CompletableFuture
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 @Aspect
 @Component
-@ConditionalOnProperty(name = ["log-method-execution"], havingValue = "true")
+@ConditionalOnProperty(name = [PROP_NAME], havingValue = PROP_VALUE)
 class MethodExecutionLoggingAspect {
 
-    @OptIn(ExperimentalTime::class)
-    @Around("webLayer() || serviceLayer() || dataLayer()")
-    fun logMethodExecution(joinPoint: ProceedingJoinPoint): Any? {
-        val logger = joinPoint.componentLogger()
-        val signature = joinPoint.signature as MethodSignature
-        val methodName = signature.name
-        val methodArgs = joinPoint.args.contentToString()
-
-        logger.debug("Method: {}() >>> args: {}", methodName, methodArgs)
-        val (result, duration) = measureTimedValue { joinPoint.proceed() }
-        logger.debug("Method: {}() <<< exit: {}, time: {}", methodName, signature.format(result), duration)
-
-        return result
+    @PostConstruct
+    fun init() {
+        if (LoggerFactory.getLogger(ROOT).isDebugEnabled) {
+            logger.info("Method execution aspect is configured to use")
+        } else {
+            logger.warn(
+                "Property '{}' has value '{}' but DEBUG level is disabled for the '{}' logger",
+                PROP_NAME,
+                PROP_VALUE,
+                ROOT,
+            )
+        }
     }
 
-    @Pointcut("execution(* $ROOT.web..*(..))" +
-        " && (within($REST_CONTROLLER *) || within($DGS_COMPONENT *) || within($DGS_DATA_LOADER *))")
-    private fun webLayer() { /* no-op */ }
+    @Around("webLayer() || serviceLayer() || dataLayer()")
+    fun logMethodExecution(joinPoint: ProceedingJoinPoint): Any? {
+        val method = joinPoint.signature as MethodSignature
+        val logger = LoggerFactory.getLogger(method.declaringTypeName)
+
+        return if (logger.isDebugEnabled) {
+            val methodName = method.name
+            val methodArgs = joinPoint.args.contentToString()
+
+            logger.debug(FUN_EXECUTION_START, methodName, methodArgs)
+
+            with(Timer()) {
+                when (val result = joinPoint.proceed()) {
+                    is CompletableFuture<*> -> result.thenApply {
+                        logger.debug(FUN_EXECUTION_CLOSE, methodName, method.format(it), elapsedTime)
+                        it
+                    }
+
+                    else -> result.let {
+                        logger.debug(FUN_EXECUTION_CLOSE, methodName, method.format(it), elapsedTime)
+                        it
+                    }
+                }
+            }
+        } else {
+            joinPoint.proceed()
+        }
+    }
+
+    @Pointcut("execution(* $ROOT.web..*(..)) && (within($REST_CONTROLLER *) || within($DGS_COMPONENT *) || within($DGS_DATA_LOADER *))")
+    private fun webLayer() {
+        /* no-op */
+    }
 
     @Pointcut("execution(* $ROOT.service..*(..))")
-    private fun serviceLayer() { /* no-op */ }
+    private fun serviceLayer() {
+        /* no-op */
+    }
 
     @Pointcut("execution(* $ROOT.data..*(..))")
-    private fun dataLayer() { /* no-op */ }
+    private fun dataLayer() {
+        /* no-op */
+    }
+
+    @JvmInline
+    value class Timer(private val start: Long = System.currentTimeMillis()) {
+
+        val elapsedTime: Duration
+            get() = (System.currentTimeMillis() - start).toDuration(DurationUnit.MILLISECONDS)
+    }
 
     companion object {
+        private const val FUN_EXECUTION_START = "Method: {}() >>> args: {}"
+        private const val FUN_EXECUTION_CLOSE = "Method: {}() <<< exit: {}, time: {}"
+
         private const val ROOT = "com.github.mburyshynets.dgs"
         private const val VOID = "void"
 
-        private const val REST_CONTROLLER = "@org.springframework.web.bind.annotation.RestController"
         private const val DGS_COMPONENT = "@com.netflix.graphql.dgs.DgsComponent"
         private const val DGS_DATA_LOADER = "@com.netflix.graphql.dgs.DgsDataLoader"
+        private const val REST_CONTROLLER = "@org.springframework.web.bind.annotation.RestController"
 
-        private fun JoinPoint.componentLogger(): Logger {
-            return LoggerFactory.getLogger(signature.declaringTypeName)
-        }
+        internal const val PROP_NAME = "log-method-execution"
+        internal const val PROP_VALUE = "true"
 
-        private fun MethodSignature.format(result: Any?): Any? {
-            return if (returnType.name == VOID) {
-                VOID
-            } else {
-                result
-            }
+        private val logger = LoggerFactory.getLogger(MethodExecutionLoggingAspect::class.java)
+
+        @Suppress("NOTHING_TO_INLINE")
+        private inline fun MethodSignature.format(result: Any?): String = when (returnType.name) {
+            VOID -> VOID
+            else -> result.toString()
         }
     }
 }
--- app-server/src/main/kotlin/com/github/mburyshynets/dgs/web/graphql/fetcher/UserFetcher.kt
+++ app-server/src/main/kotlin/com/github/mburyshynets/dgs/web/graphql/fetcher/UserFetcher.kt
@@ -8,36 +8,29 @@ import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 
 @DgsComponent
 class UserFetcher(private val userService: UserService) {
 
-    @DgsQuery
-    @PreAuthorize("isAuthenticated()")
-    fun users(): List<User> {
-        return userService.getAllUsers()
-    }
+    @DgsMutation
+    fun createUser(@InputArgument request: CreateUserRequest): User =
+        userService.createNewUser(request)
 
     @DgsQuery
-    @PreAuthorize("isAuthenticated()")
-    fun user(@InputArgument username: String): User {
-        return userService.getUserByUsername(username)
-    }
+    fun users(): List<User> =
+        userService.getAllUsers()
 
     @DgsQuery
-    fun currentUser(@AuthenticationPrincipal currentUser: ExtendedUserDetails?): User? {
-        return currentUser?.let {
+    fun user(@InputArgument username: String): User =
+        userService.getUserByUsername(username)
+
+    @DgsQuery
+    fun currentUser(@AuthenticationPrincipal currentUser: ExtendedUserDetails?): User? =
+        currentUser?.let {
             User(
                 id = it.id.toString(),
                 username = it.username,
             )
         }
-    }
-
-    @DgsMutation
-    fun createUser(@InputArgument request: CreateUserRequest): User {
-        return userService.createNewUser(request)
-    }
 }
